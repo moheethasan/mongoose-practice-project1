62fcc933742ff507c9240fc11b581f05ad927bce
majesty-01 <moheethasan4@gmail.com>
2024-07-03 00:40:00 +0600

    added student features

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    // call service function to send the data
+    const result = await StudentServices.createStudentIntoDB(studentData);
+    // send response
+    res.status(200).json({
+      success: true,
+      message: "student is created successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "student are retrieved successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSignleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: "student is retrieved successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSignleStudent,
+};
